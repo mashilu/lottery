89d5dc0d59541028c792fe3cdab27f85349bfc3e
mashilu <mslbuaa@163.com>
2016-04-14 18:58:03 +0800

    按照PEP8规范更新代码

--- lottery.py
+++ lottery.py
@@ -2,99 +2,106 @@ from __future__ import division
 
 from itertools import combinations
 
+
 # 等差数列
-def arithProgress(aList):
-	count = len(aList)
-	step = aList[1] - aList[0]
+def arith_progress(alist):
+	count = len(alist)
+	step = alist[1] - alist[0]
 	for i in range(1, count):
-		if (aList[i] - aList[i-1] == step):
+		if alist[i] - alist[i-1] == step:
 			continue
 		else:
 			return False
-	return	True
+	return True
+
 
 # 等比数列
-def GeomProgress(aList):
-	count = len(aList)
-	step = aList[1] / aList[0]
+def geom_progress(alist):
+	count = len(alist)
+	step = alist[1] / alist[0]
 	for i in range(1, count):
-		if (aList[i] / aList[i-1] == step):
+		if alist[i] / alist[i-1] == step:
 			continue
 		else:
 			return False	
-	return	True
+	return True
+
 
 # 全偶数
-def allEven(aList):
-	for i in aList:
-		if (i % 2 != 0):
+def all_even(alist):
+	for i in alist:
+		if i % 2 != 0:
 			return False
 	return True
 
-#全奇数 
-def allOdd(aList):
-	for i in aList:
-		if (i % 2 == 0):
+
+# 全奇数
+def all_odd(alist):
+	for i in alist:
+		if i % 2 == 0:
 			return False
 	return True
+
 
 # 5连环
-def connect5(aList):
-	a1 = [aList[0], aList[1], aList[2], aList[3], aList[4]]
-	a2 = [aList[1], aList[2], aList[3], aList[4], aList[5]]
-	if (arithProgress(a1) or GeomProgress(a1) or
-		arithProgress(a2) or GeomProgress(a2)):
+def connect5(alist):
+	a1 = [alist[0], alist[1], alist[2], alist[3], alist[4]]
+	a2 = [alist[1], alist[2], alist[3], alist[4], alist[5]]
+	if arith_progress(a1) or geom_progress(a1) or \
+		arith_progress(a2) or geom_progress(a2):
 		return True
 	return False
+
 
 # 4连环
-def connect4(aList):
-	a1 = [aList[0], aList[1], aList[2], aList[3]]
-	a2 = [aList[1], aList[2], aList[3], aList[4]]
-	a3 = [aList[2], aList[3], aList[4], aList[5]]
+def connect4(alist):
+	a1 = [alist[0], alist[1], alist[2], alist[3]]
+	a2 = [alist[1], alist[2], alist[3], alist[4]]
+	a3 = [alist[2], alist[3], alist[4], alist[5]]
 	
-	if (arithProgress(a1) or GeomProgress(a1) or
-		arithProgress(a2) or GeomProgress(a2) or
-		arithProgress(a3) or GeomProgress(a3)):
+	if arith_progress(a1) or geom_progress(a1) or arith_progress(a2) or \
+		geom_progress(a2) or arith_progress(a3) or geom_progress(a3):
 		return True
 	return False
+
 
 # 3连环
-def connect3(aList):
-	a1 = [aList[0], aList[1], aList[2]]
-	a2 = [aList[1], aList[2], aList[3]]
-	a3 = [aList[2], aList[3], aList[4]]
-	a4 = [aList[3], aList[4], aList[5]]
+def connect3(alist):
+	a1 = [alist[0], alist[1], alist[2]]
+	a2 = [alist[1], alist[2], alist[3]]
+	a3 = [alist[2], alist[3], alist[4]]
+	a4 = [alist[3], alist[4], alist[5]]
 	
-	if (arithProgress(a1) or GeomProgress(a1) or
-		arithProgress(a2) or GeomProgress(a2) or
-		arithProgress(a3) or GeomProgress(a3) or
-		arithProgress(a4) or GeomProgress(a4)):
+	if arith_progress(a1) or geom_progress(a1) or \
+		arith_progress(a2) or geom_progress(a2) or \
+		arith_progress(a3) or geom_progress(a3) or \
+		arith_progress(a4) or geom_progress(a4):
 		return True
 	return False
 
+
 # 2连环
-def connect2(aList):
-	if ((aList[1] - aList[0] == 1) or
-		(aList[2] - aList[1] == 1) or
-		(aList[3] - aList[2] == 1) or
-		(aList[4] - aList[3] == 1) or
-		(aList[5] - aList[4] == 1)):
+def connect2(alist):
+	if (alist[1] - alist[0] == 1) or (alist[2] - alist[1] == 1) or \
+		(alist[3] - alist[2] == 1) or (alist[4] - alist[3] == 1) or \
+		(alist[5] - alist[4] == 1):
 		return True
 		
 	return False
 
-# 全大于10
-def allBig10(aList):
 
-	if (aList[0] > 10):
+# 全大于10
+def all_big10(alist):
+
+	if alist[0] > 10:
 		return True
 	else:
 		return False
 
+
 # 全小于10
-def allSmall10(aList):
-	if (aList[5] < 10):
+def all_small10(alist):
+	if alist[5] < 10:
 		return True
 	else:
 		return False
@@ -102,10 +109,8 @@ def allSmall10(aList):
 fp = open("total.txt", 'w')
 
 for i in combinations(range(1, 34), 6):
-	if (arithProgress(i) or GeomProgress(i) or 
-		allEven(i) or allOdd(i) or 
-		connect5(i) or connect4(i) or connect3(i) or connect2(i)
-		or allBig10(i) or allSmall10(i)):
+	if arith_progress(i) or geom_progress(i) or all_even(i) or all_odd(i) or \
+		connect5(i) or connect4(i) or connect3(i) or connect2(i) or all_big10(i) or all_small10(i):
 		continue
 	else:
 		for ele in i:
