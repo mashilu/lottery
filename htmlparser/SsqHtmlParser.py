c9d44f75d504a92b412500ada4778a35ab0df2f3
mashilu <mslbuaa@163.com>
2016-04-17 22:09:56 +0800

    优化ssq代码，独立数据爬取和数据库操作代码

---
+++ htmlparser/SsqHtmlParser.py
@@ -0,0 +1,43 @@
+from html.parser import HTMLParser
+
+
+class SsqHTMLParser(HTMLParser):
+	def error(self, message):
+		pass
+
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.is_redspan = ""
+		self.is_bluespan = ""
+		self.is_serialspan = ""
+		self.cur_result = []
+		self.result = []
+
+	def handle_starttag(self, tag, attrs):
+		if tag == "span":
+			if attrs[0][0] == "class" and attrs[0][1] == "STYLE13":
+				self.is_redspan = 1
+			if attrs[0][0] == "class" and attrs[0][1] == "STYLE12":
+				self.is_bluespan = 1
+			if attrs[0][0] == "class" and attrs[0][1] == "end":
+				self.is_serialspan = 1
+
+	def handle_endtag(self, tag):
+		if tag == "span":
+			if self.is_redspan == 1:
+				self.is_redspan = ""
+			if self.is_bluespan == 1:
+				self.is_bluespan = ""
+			if self.is_serialspan == 1:
+				self.is_serialspan = ""
+
+	def handle_data(self, data):
+		if self.is_serialspan == 1:
+			self.cur_result.append(data.strip())
+		if self.is_redspan == 1:
+			for a_ball in sorted(data.strip().split()):
+				self.cur_result.append(a_ball)
+		if self.is_bluespan == 1:
+			self.cur_result.append(data.strip())
+			self.result.append(self.cur_result)
+			self.cur_result = []
---
+++ lottery-ssqhis_db.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+
+import urllib.request
+
+from database.mysqldb import MySqlDb
+from htmlparser.SsqHtmlParser import SsqHTMLParser
+
+
+def truncate_table(dbconn, table_name):
+	sql = "truncate table " + table_name
+	dbconn.exe_sql(sql)
+
+
+def save_data(dbconn, result):
+	# insert new data
+	for curr_result in result:
+		sql_insert = "insert into lottery.ssqHistory(no, red1, \
+				red2, red3, red4, red5, red6, blue1) \
+				values(%s, %s, %s, %s, %s, %s, %s, %s)" % tuple(curr_result)
+		dbconn.exe_insert(sql_insert)
+
+
+def check_data(dbconn, count):
+	# check number of data inserted with count
+	sql_count = "select count(1) from lottery.ssqHistory"
+	dbconn.exe_sql(sql_count)
+	# return type of fetch_one is tuple
+	dbcnt = dbconn.fetch_one()[0]
+	print(dbcnt, count)
+	if count == dbcnt:
+		print("insert operation is finished successfully")
+	else:
+		print("insert operation is finished with error")
+
+if __name__ == "__main__":
+	base_url = "http://www.3dcp.cn/zs/gonggao.php?type=ssq&year="
+
+	db = MySqlDb(host='localhost', port=3306, user='root', passwd='root', db='lottery', charset='UTF8')
+	db.conndb()
+	truncate_table(db, "lottery.ssqHistory")
+
+	# total record quantity
+	get_count = 0
+	for url in [base_url + str(x) for x in range(2002, 2017, 1)]:
+		content = urllib.request.urlopen(url).read()
+		ball = SsqHTMLParser()
+		ball.feed(content.decode("GB2312"))
+		save_data(db, ball.result)
+		get_count += len(ball.result)
+
+	check_data(db, get_count)
+
+	db.close()
